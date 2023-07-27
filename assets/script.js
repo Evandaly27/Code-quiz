a55112c67f9ce571e9b05c8398f931ccc30c76e3
Evand27 <evan.daly1127@gmail.com>
2023-07-26 18:09:07 -0600

    Timer added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
 // Variables for quiz
-var timeLeft = 90;
+var timeLeft = document.getElementById("timeLeft")
+var timerEl = document.getElementById("timer")
+var startButton = document.getElementById("start")
 
+function timeTick() { // Timer function
+    timeLeft--;
+    timerEl.textContent = "Time: + timeLeft";
+    if (timeLeft <= 0) {
+        saveScore();
+    }
+}

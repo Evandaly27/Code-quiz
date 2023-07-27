b0927176efbf554e633d7ba7be88c2946afb62f6
Evand27 <evan.daly1127@gmail.com>
2023-07-26 19:41:50 -0600

    Updated html for new buttons

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var timeLeft = document.getElementById("timeLeft")
 var timerEl = document.getElementById("timer")
 var startButton = document.getElementById("start")
 
-function timeTick() { // Timer function
+function timeTick() { // Timer f
     timeLeft--;
     timerEl.textContent = "Time: + timeLeft";
     if (timeLeft <= 0) {

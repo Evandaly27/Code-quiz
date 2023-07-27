68fd35dde07a2f870aace47fc7ef317b1f3adeda
Evand27 <evan.daly1127@gmail.com>
2023-07-27 17:01:11 -0600

    added show highscores and start timer function

--- assets/script.js
+++ assets/script.js
@@ -121,3 +121,62 @@ function showNextQuestion() { // Function to show the next question or final res
         }
     }
 }
+
+function showResults() {
+    quizContainer.style.display = 'none'; // hide the part that shows the questions and answers
+    submitButton.style.display = 'none'; // hides the submit button 
+    timerContainer.style.display = 'none'; // hides the timer
+    scoreContainer.style.display = 'none'; // hides the users score
+
+    finalScoreDisplay.textContent = score; // shows the users final score
+    totalQuestionsDisplay.textContent = quizData.length; // shows the total number of questions
+
+    resultContainer.style.display = 'block'; // shows the final scores after the quiz
+
+    const initialsForm = document.getElementById('initials-form'); // grabs the element where the user enters their initials 
+    initialsForm.style.display = 'block'; // makes the form visible  
+    
+    saveScoreButton.addEventListener('click', function () { 
+        const initials = initialsInput.value.trim(); // get the initials entered and removes extra spaces
+
+        if (initials !== '') { // if the user enters their initials it will save the score 
+            const highScores = JSON.parse(localStorage.getItem('highscores')) || []; // retrieves the prevouse highscores
+            highScores.push({ initials, score }); // adds the users initials to the list of highscores
+            localStorage.setItem('highScores', JSON.stringify(highScores)); // sasves the updated scores to local storage
+
+            alert('Score saved successfully!'); // tells the user their initials were saved 
+        } else {
+            alert('Please enter your initials to save your score.'); // if users does not enter initials it shows this alert
+        }
+    });
+}
+
+function showHighscores() { // displays the list of high scores on the page 
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || []; // retrieves high scores from local storage 
+    if (highScores.length === 0) { // checks if there are no high scores
+        alert('No high scores found.'); // if no high scores this alert will pop up
+    } else {
+        highscoreList.innerHTML = ''; // if no high scores this will clear the current list of displays 
+        highScores.forEach((entry, index) => { // loops through the high score entries and displays them
+            const listItem = document.createElement('li'); // creates new list item for the high scores
+            listItem.textContent = `${index + 1}. ${entry.initials}: ${entry.score}`; // shows the list of initials and scores
+            highscoreList.appendChild(listItem); //adds the high score to the list
+        });
+        highscoresContainer.style.display = 'block'; // displays the list 
+    }
+}
+
+viewHighscoresButton.addEventListener('click', showHighscores);
+
+function startTimer() {
+    const timerInterval = setInterval(() => {
+        timeLeft--;
+        timeRemaining.textContent = timeLeft;
+        currentScore.textContent = score;
+
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            showResults();
+        }
+    }, 1000);
+}

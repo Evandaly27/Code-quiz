51a023ec21a98a6ef1cd01fb4f6ec0c03567c626
Evand27 <evan.daly1127@gmail.com>
2023-07-26 19:58:40 -0600

    Variables added

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
-// Variables for quiz
-var timeLeft = document.getElementById("timeLeft")
-var timerEl = document.getElementById("timer")
-var startButton = document.getElementById("start")
-
-function timeTick() { // Timer f
-    timeLeft--;
-    timerEl.textContent = "Time: + timeLeft";
-    if (timeLeft <= 0) {
-        saveScore();
-    }
-}
+// constant variables 
+const startButton = document.getElementById('start-btn');
+const quizContainer = document.getElementById('quiz');
+const submitButton = document.getElementById('submit-btn');
+const resultContainer = document.getElementById('result');
+const timerContainer = document.getElementById('timer');
+const timeRemaining = document.getElementById('time-remaining');
+const scoreContainer = document.getElementById('score');
+const currentScore = document.getElementById('current-score');
+const finalScoreDisplay = document.getElementById('final-score');
+const totalQuestionsDisplay = document.getElementById('total-questions');
+const initialsInput = document.getElementById('initials');
+const saveScoreButton = document.getElementById('save-score-btn');
+const viewHighscoresButton = document.getElementById('view-highscores-btn');
+const highscoresContainer = document.getElementById('highscores');
+const highscoreList = document.getElementById('highscore-list');
